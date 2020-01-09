2890305cc1251cfa911700dc87bce770acef82eb
keiSunagawa <keisunagawa.git@icloud.com>
2020-01-09 21:02:14 +0900

    create file

--- core/src/main/scala/me/kerfume/ypsctem/KPlugin.scala
+++ core/src/main/scala/me/kerfume/ypsctem/KPlugin.scala
@@ -18,26 +18,53 @@ object KPlugin extends AutoPlugin {
   override def projectSettings: Seq[Def.Setting[_]] =
     super.projectSettings ++ Def.settings(
       sctem := {
-        val args: Seq[String] = spaceDelimited("<arg>").parsed
-        // '=' でsplitして各値にパース
-        val pj = args(0)
-        val pkg = args(1)
-        val template = args(2)
-        val valuesJson = args(3)
+        try {
+          val args: Seq[String] = spaceDelimited("<arg>").parsed
+          val parsedArgs = args.map { a =>
+            val kv = a.split("=")
+            kv(0) -> kv(1)
+          }.toMap
+          // '=' でsplitして各値にパース
+          val pj = parsedArgs.getOrElse("prj", "root")
+          val scope = parsedArgs.getOrElse("scp", "main")
+          val codeType = parsedArgs.getOrElse("ctp", "scala")
+          val dest = parsedArgs.getOrElse("dst", "com.example.Hello")
+          val template = parsedArgs.getOrElse("tmp", "noop")
+          val valuesJson = parsedArgs.getOrElse("val", "{}")
+          println(valuesJson)
 
+          val pkg = dest.split("\\.").init
+          val fileNameBase = dest.split("\\.").last
 
-        // target projectのディレクトリ取得
-        val targetPj = Project.extract(state.value).structure.allProjectPairs.find {case (_, ref) =>
-          ref.project == pj
-        }.map(_._1.base)
+          // target projectのディレクトリ取得
+          val targetPj = Project
+            .extract(state.value)
+            .structure
+            .allProjectPairs
+            .find {
+              case (_, ref) =>
+                ref.project == pj
+            }
+            .map(_._1.base)
+            .get // TODO unsafe
 
-        // template に valuesJson を apply
+          // template に valuesJson を apply TODO
+          val content =
+            s"""package ${pkg.mkString(".")}
+             |class ${fileNameBase} {}
+             |""".stripMargin
 
-        // package ディレクトリ作成(main/scalaのみ)
+          // package ディレクトリ作成(main/scalaのみ)
+          val dir = s"${targetPj}/src/${scope}/${codeType}/${pkg.mkString("/")}"
+          IO.createDirectory(new File(dir))
 
-        // 作成したpackageディレクトリにtemplateにvalueを適用した.scalaファイルを設置
+          // 作成したpackageディレクトリにtemplateにvalueを適用した.scalaファイルを設置
+          IO.write(new File(s"${dir}/${fileNameBase}.scala"), content, IO.utf8)
+        } catch {
+          case e: Throwable =>
+            e.printStackTrace()
+        }
 
-        println(args)
       }
 //    commands += Command.args("sctemWith", "") {
 //      case (s, args) =>
